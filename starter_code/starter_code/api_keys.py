075c2a9f095dd0bcf6371ffd9c320b4137abe2d5
Wasif Khan <wkhan.cpa@gmail.com>
2020-12-29 11:41:37 -0500

    Update api_keys.py

--- starter_code/starter_code/api_keys.py
+++ starter_code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "6b0bad7c1503269f2aa5ba0cadc0d7a5"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyBAFxlwJ_s_oYNh0IopnsjO4XFyPJUr8iM"
+g_key = ""
